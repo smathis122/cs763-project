b1e414de7021984ec6f06fb8061a97d988e0241d
Saahil Vashishta <saahilv@bu.edu>
2023-09-24 13:09:05 -0700

    Clean up and commenting code
    Clean up and commenting code

--- code/Backend/app.py
+++ code/Backend/app.py
@@ -29,77 +29,12 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
+# Database table for user
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
 
-# up.uses_netloc.append("postgres")
-# url = up.urlparse(os.environ["DATABASE_URL"])
-# conn = psycopg2.connect(database=url.path[1:],
-# user=url.username,
-# password=url.password,
-# host=url.hostname,
-# port=url.port
-# )
-# db = SQLAlchemy()
-# ma = Marshmallow(app)
-
-# #Question for team: Might not be the best idea to create a table here, should need a separate file for that or create it directly in our database
-# class User(db.Model):
-#     """Creating database for user"""
-
-#     __tablename__ = "User"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(35), unique=True, nullable=False)
-#     emailID = db.Column(db.String(35), unique=True, nullable=False)
-#     password = db.Column(db.String())
-#     type = db.Column(db.Integer)
-
-
-
-# def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
-
-# class UserSchema(ma.Schema):
-#     class Meta:
-#         fields =("id", "username")
-
-# user_schema = UserSchema()
-# multiple_user_schema = UserSchema(many=True)
-
-# @app.route('/registerUser', methods=['POST'])
-# def registerNewUserEmail():
-#     try: 
-#         #parse json data
-#         userData = request.get_json()
-#         print(userData)
-#         #defining cursor to postgres data
-#         cursor = conn.cursor()
-#         #Hashing the password
-#         password = userData["password"]
-#         passwordCrypt = b"{password}"
-#         salt = bcrypt.gensalt()
-#         hashed = bcrypt.hashpw(passwordCrypt, salt)
-
-#         query_email_exists = "SELECT EXISTS ( SELECT 1 FROM User WHERE user_email = '{emailID}')"#might need AS email_exists after) , need to check
-#         if  query_email_exists:
-        
-#             insertion_query = "INSERT INTO User (name, emailID, password, type) VALUES (%s, %s, %s, %d)"
-
-#             cursor.execute(insertion_query, (userData["name"], userData["emailID"], userData["hashed"], userData["type"]))
-
-#             conn.commit()
-#             cursor.close()
-#             conn.close()
-#             return jsonify({"message": "Equipment added successfully"}), 201
-#         print("Email ID already exists in database")
-"""
-Need a pop-up here for UI
-"""
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 500
 #Configuration
 #Application Configuration for Client ID, Client Secret, URL, and Flask Secret, testing on a local port.
 appConf = {
@@ -131,25 +66,30 @@ def home():
         return render_template("home.html", session=session.get("user"),
                                pretty=json.dumps(session.get("user"), indent=4))
 
+# Logic for logout 
 @app.route("/logout")
 def logout():
     session.pop("user", None)
     return redirect(url_for("home"))
 
+# Logic for Google login 
 @app.route("/google-login")
 def googleLogin():
       return oauth.GearToGoApp.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
 
+# Logic for Google sign-in 
 @app.route("/signin-google")
 def googleCallback():
     token = oauth.GearToGoApp.authorize_access_token()
     user_info = token.get('userinfo', None)
     user_email = user_info.email
+    #Tech debt to be implemented later
     user_birthday = token.get('birthdate', None)
-    print(user_email)
     session["user"] = token
+    #logic to check database for matching email
     if User.query.filter_by(email=user_email).first():
          return redirect(url_for("home"))
+    #Logic to add to database
     else:
          new_user = User(email=user_email, password="Google account, password not available")
          db.session.add(new_user)
