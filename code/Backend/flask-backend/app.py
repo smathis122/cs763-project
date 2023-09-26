641f176552779362263c0902768e69c05d79abd8
smathis122 <smathis@bu.edu>
2023-09-25 20:09:41 -0700

    SM: fixed merge issues

--- code/Backend/flask-backend/app.py
+++ code/Backend/flask-backend/app.py
@@ -1,6 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
-
+from oauthlib.oauth2 import WebApplicationClient
+from authlib.integrations.flask_client import OAuth 
+import requests
+import json
+import urllib
+import urllib.parse as up
 from flask import Flask, render_template, url_for, redirect, flash, request, jsonify
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
@@ -10,20 +15,51 @@ import re
 # from flask_bcrypt import Bcrypt
 import os
 import secrets
-from dotenv import load_dotenv
 import binascii
 import bcrypt
 import psycopg2
 from flask_cors import CORS
 from flask_wtf.csrf import generate_csrf
-
+from dotenv import load_dotenv
 app = Flask(__name__)
-load_dotenv()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.secret_key = os.environ.get('SECRET_KEY')
+DATABASE_URL ="postgresql://fcfcgjwl:Eb5MNeBN-fJlmTipRgqaC-c0tzO3gM5r@bubble.db.elephantsql.com/fcfcgjwl"
+SECRET_KEY = "SECRET123456"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.secret_key = SECRET_KEY
 
 db = SQLAlchemy(app)
+
+# Database table for user
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+#Configuration
+#Application Configuration for Client ID, Client Secret, URL, and Flask Secret, testing on a local port.
+appConf = {
+    "OAUTH_CLIENT_ID" : "95479501580-7om6n792bbd8em6l76a2sf14a8dg2h80.apps.googleusercontent.com",
+    "OAUTH_CLIENT_SECRET" : "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",
+    "OAUTH_META_URL" : "https://accounts.google.com/.well-known/openid-configuration",
+    "FLASK_SECRET" : "77c7e3f1-0128-4408-9081-0e014e8bcabf",
+    "FLASK_PORT" : 5008
+    }   
+oauth = OAuth(app)
+
+#Adding Secret Key
+app.secret_key = appConf.get("FLASK_SECRET")
+
+oauth.register("GearToGoApp",
+               client_id = appConf.get("OAUTH_CLIENT_ID"),
+               client_secret = appConf.get("OAUTH_CLIENT_SECRET"),
+               server_metadata_url=appConf.get("OAUTH_META_URL"),
+               client_kwargs = {
+                     "scope":"openid profile email https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/user.gender.read",
+               }
+               )
+
 
 CORS(app) # Allow all origins for development; restrict in production
 
@@ -439,8 +475,40 @@ def get_unavailable_items():
         return jsonify({"error": str(e)}), 500
 
 
-#Main method
+# Logic for logout 
+@app.route("/google-logout")
+def googleLogout():
+    session.pop("user", None)
+    return redirect(url_for("home"))
+
+# Logic for Google login 
+@app.route("/google-login")
+def googleLogin():
+      return oauth.GearToGoApp.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
+
+# Logic for Google sign-in 
+@app.route("/signin-google")
+def googleCallback():
+    token = oauth.GearToGoApp.authorize_access_token()
+    user_info = token.get('userinfo', None)
+    user_email = user_info.email
+    #Tech debt to be implemented later
+    user_birthday = token.get('birthdate', None)
+    session["user"] = token
+    #logic to check database for matching email
+    if User.query.filter_by(email=user_email).first():
+         return redirect(url_for("home"))
+    #Logic to add to database
+    else:
+         new_user = User(email=user_email, password="Google account, password not available")
+         db.session.add(new_user)
+         db.session.commit()
+         return redirect(url_for("home"))
+
+
+
+    #Main method
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=conf.get("FLASK_PORT"), debug=True)
+    app.run(host="0.0.0.0", port=appConf.get("FLASK_PORT"), debug=True)
 
 
