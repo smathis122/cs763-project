4d44350a7c05beac070745dbbe98f5fe5a0454fc
Saahil Vashishta <97310378+saahilv@users.noreply.github.com>
2023-10-06 23:49:42 -0700

    Addition of image to About Us page

--- code/Frontend/react-frontend/src/Components/AboutUs.js
+++ code/Frontend/react-frontend/src/Components/AboutUs.js
@@ -1,14 +1,40 @@
 import React from "react";
+import "../styles/Components/aboutus.css";
 
 function AboutUs() {
     return (
-        <div>
+        <div className="container">
+            <img className ="logo" src={require("../Media/GearOnTheGo.png")} alt="Gear On The Go" />
             <br />
-            <h2>Our Story</h2>
+            <h2>Step into a world of adventure with Gear On the Go! </h2>
             <p>
-                Hello everyone, welcome to the web Application for Gear to Go. Our team met as part of a 
-                Software Engineering class at Boston University and decided to work on an application that helps 
-                
+                Hey there, thrill-seekers and outdoor enthusiasts! Get ready to embark on a game-changing
+                journey with our cutting-edge web application. Picture this: a dynamic team forged in the
+                crucible of Boston University's Software Engineering class, driven by the mission to revolutionize
+                how we experience the great outdoors!
+            </p>
+            <br />
+            <p>
+                Introducing Gear On The Go—an ingenious platform connecting owners of top-notch recreational gear,
+                from sleek paddleboards to rugged hiking equipment, and even high-performance bikes! Imagine
+                having the freedom to pursue your passions without the hassle of ownership. Whether you're a casual explorer or a
+                globe-trotting adventurer, we've got you covered.
+            </p>
+            <br />
+            <p>
+                Why buy when you can experience the best without the commitment? Our platform brings together equipment owners
+                looking to share their treasures during downtime. And for those on the move or seeking an adrenaline rush,
+                Gear to Go unlocks access to top-tier gear, right when you need it.
+            </p>
+            <br />
+            <p>
+                Seize this golden opportunity to turn your unused equipment into a thriving business, or dive headfirst
+                into a world of endless possibilities as a renter. Gear On The Go is not just an application; it's a lifestyle,
+                an experience, a chance to embrace the great outdoors like never before!
+            </p>
+            <br />
+            <p>
+                Gear up for a revolution—Gear up for Gear On The Go! 🚀🔥
             </p>
         </div>
     );
