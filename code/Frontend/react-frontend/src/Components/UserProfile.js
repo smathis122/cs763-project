d5cfa7e68d5f4d602d5432b11284ae8beb8f2498
smathis122 <smathis@bu.edu>
2023-10-11 20:13:59 -0700

    SM: make unavailable not available to reserve from profile page

--- code/Frontend/react-frontend/src/Components/UserProfile.js
+++ code/Frontend/react-frontend/src/Components/UserProfile.js
@@ -111,45 +111,53 @@ function UserProfile() {
   return (
     <div>
       <NavbarCustom />
-      <Row>
-        <Col md={6}>
-          <h2
-            style={{
-              marginLeft: "15px",
-              marginTop: "20px",
-            }}
-          >
-            {usernameSelected}
-          </h2>
-        </Col>
-        <Col md={6} className="text-right">
-          <Button
-            variant="secondary" // Set the desired Bootstrap button variant
-            onClick={() => handleProfileClick()} // Replace handleNewButtonClick with your logic
-            style={{
-              fontSize: "20px",
-              width: "100%",
-              marginRight: "5px",
-              marginTop: "15px",
-              marginBottom: "25px",
-            }}
-          >
-            View other Profiles
-          </Button>
-        </Col>
-      </Row>
-      <Container fluid>
+      <div
+        style={{
+          marginLeft: "10px",
+          marginRight: "10px",
+        }}
+      >
         <Row>
-          <Col md={8} className="items-hosted-column">
-            <h3>Items Hosted</h3>
-
-            <Row>
-              {equipmentData &&
-              equipmentData.items &&
-              equipmentData.items.length > 0 ? (
-                equipmentData.items.map(
-                  (item) =>
-                    item.owner === usernameSelected && ( // Filter items for the selected user
+          <Col md={8}>
+            <h2
+              style={{
+                marginLeft: "15px",
+                marginTop: "20px",
+              }}
+            >
+              {usernameSelected}
+            </h2>
+          </Col>
+          <Col md={4} className="text-right">
+            <Button
+              variant="secondary"
+              onClick={() => handleProfileClick()}
+              style={{
+                fontSize: "20px",
+                width: "100%",
+                marginRight: "10px",
+                marginTop: "15px",
+                marginBottom: "25px",
+              }}
+            >
+              View other Profiles
+            </Button>
+          </Col>
+        </Row>
+        <Container fluid>
+          <Row>
+            <Col md={8} className="items-hosted-column">
+              <h3>Available Items</h3>
+              <Row>
+                {equipmentData &&
+                equipmentData.items &&
+                equipmentData.items.length > 0 ? (
+                  equipmentData.items
+                    .filter(
+                      (item) =>
+                        item.owner === usernameSelected && item.available
+                    )
+                    .map((item) => (
                       <Col key={item.id} className="col-md-4 col-12">
                         <Card
                           className="custom-card"
@@ -167,116 +175,159 @@ function UserProfile() {
                           </Card.Body>
                         </Card>
                       </Col>
-                    )
-                )
-              ) : (
-                <p>{usernameSelected} has no equipment items hosted.</p>
-              )}
-            </Row>
-          </Col>
-          <Col md={4} className="reviews-reservations-column">
-            <Row>
-              <Col md={6}>
-                <h3>Reviews</h3>
-              </Col>
-              <Col md={6} className="text-right">
-                <Button
-                  style={{ width: "100%" }}
-                  variant="primary"
-                  onClick={() => setShowReviewModal(true)}
-                >
-                  Write Review
-                </Button>
-              </Col>
-            </Row>
-            {reviews.length === 0 ? (
-              <p>This person has no reviews yet.</p>
-            ) : (
-              reviews.map((review, index) => (
-                <Card key={index} style={{ marginBottom: "10px" }}>
-                  <Card.Body>
-                    <Card.Title>{review[3]}</Card.Title>
-                    <Card.Text>Rating: {review[4]}</Card.Text>
-                    <Card.Text>{review[5]}</Card.Text>
-                    <Card.Text>By: {review[2]}</Card.Text>
-                  </Card.Body>
-                </Card>
-              ))
-            )}
-          </Col>
-        </Row>
-      </Container>
-      {/* Modal for displaying details */}
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>{selectedItem?.name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Status: {selectedItem?.available ? "Available" : "Unavailable"}</p>
-          <p>Description: {selectedItem?.description}</p>
-          <p>Price: {selectedItem?.price}</p>
-          <p>Owner: {selectedItem?.owner}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleReserveClick}>Reserve</Button>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      {/* Add a "Write Review" Button */}
-      <Modal show={showReviewModal} onHide={() => setShowReviewModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Write a Review</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form onSubmit={handleReviewSubmit}>
-            <FormGroup className="contact-page-form-group">
-              <Form.Label className="form-label">Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Title"
-                name="title"
-                value={formReviewData.title}
-                onChange={handleInputChange}
-                required
-              />
-            </FormGroup>
-            <FormGroup className="contact-page-form-group">
-              <Form.Label className="form-label">Rating</Form.Label>
-              <Form.Control
-                as="select" // Use a <select> element for the dropdown
-                name="rating"
-                value={formReviewData.rating}
-                onChange={handleInputChange}
-                required
-              >
-                <option value="">Select Rating</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </Form.Control>
-            </FormGroup>
-            <Form.Group controlId="formReviewText">
-              <Form.Label>Review Text</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={4}
-                name="description"
-                value={formReviewData.description}
-                onChange={handleInputChange}
-                required
-              />
-            </Form.Group>
+                    ))
+                ) : (
+                  <p>
+                    {usernameSelected} has no available equipment items hosted.
+                  </p>
+                )}
+              </Row>
 
-            <Button variant="primary" type="submit">
-              Submit Review
+              <h3>Unavailable Items</h3>
+              <Row>
+                {equipmentData &&
+                equipmentData.items &&
+                equipmentData.items.length > 0 ? (
+                  equipmentData.items
+                    .filter(
+                      (item) =>
+                        item.owner === usernameSelected && !item.available
+                    )
+                    .map((item) => (
+                      <Col key={item.id} className="col-md-4 col-12">
+                        <Card
+                          className="custom-card"
+                          onClick={() => handleCardClick(item)}
+                          style={{ cursor: "pointer" }}
+                        >
+                          <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                              Status: {item.status}
+                            </Card.Subtitle>
+                            <Card.Text>{item.description}</Card.Text>
+                            <Card.Text>Price: ${item.price}</Card.Text>
+                            <Card.Text>Owner: {item.owner}</Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    ))
+                ) : (
+                  <p>
+                    {usernameSelected} has no unavailable equipment items
+                    hosted.
+                  </p>
+                )}
+              </Row>
+            </Col>
+            <Col md={4} className="reviews-reservations-column">
+              <Row>
+                <Col md={6}>
+                  <h3>Reviews</h3>
+                </Col>
+                <Col md={6} className="text-right">
+                  <Button
+                    style={{ width: "100%" }}
+                    variant="primary"
+                    onClick={() => setShowReviewModal(true)}
+                  >
+                    Write Review
+                  </Button>
+                </Col>
+              </Row>
+              {reviews.length === 0 ? (
+                <p>This person has no reviews yet.</p>
+              ) : (
+                reviews.map((review, index) => (
+                  <Card key={index} style={{ marginBottom: "10px" }}>
+                    <Card.Body>
+                      <Card.Title>{review[3]}</Card.Title>
+                      <Card.Text>Rating: {review[4]}</Card.Text>
+                      <Card.Text>{review[5]}</Card.Text>
+                      <Card.Text>By: {review[2]}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                ))
+              )}
+            </Col>
+          </Row>
+        </Container>
+        {/* Modal for displaying details */}
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedItem?.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Status: {selectedItem?.available ? "Available" : "Unavailable"}
+            </p>
+            <p>Description: {selectedItem?.description}</p>
+            <p>Price: {selectedItem?.price}</p>
+            <p>Owner: {selectedItem?.owner}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            {selectedItem?.available && (
+              <Button onClick={handleReserveClick}>Reserve</Button>
+            )}
+            <Button variant="secondary" onClick={() => setShowModal(false)}>
+              Close
             </Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
+          </Modal.Footer>
+        </Modal>
+        {/* Add a "Write Review" Button */}
+        <Modal show={showReviewModal} onHide={() => setShowReviewModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Write a Review</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={handleReviewSubmit}>
+              <FormGroup className="contact-page-form-group">
+                <Form.Label className="form-label">Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Title"
+                  name="title"
+                  value={formReviewData.title}
+                  onChange={handleInputChange}
+                  required
+                />
+              </FormGroup>
+              <FormGroup className="contact-page-form-group">
+                <Form.Label className="form-label">Rating</Form.Label>
+                <Form.Control
+                  as="select" // Use a <select> element for the dropdown
+                  name="rating"
+                  value={formReviewData.rating}
+                  onChange={handleInputChange}
+                  required
+                >
+                  <option value="">Select Rating</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </Form.Control>
+              </FormGroup>
+              <Form.Group controlId="formReviewText">
+                <Form.Label>Review Text</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={4}
+                  name="description"
+                  value={formReviewData.description}
+                  onChange={handleInputChange}
+                  required
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit Review
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </div>
     </div>
   );
 }
