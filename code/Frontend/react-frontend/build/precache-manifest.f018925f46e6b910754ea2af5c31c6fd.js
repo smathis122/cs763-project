8f7a2338b1ac30470fe097b4dc3b3c915bf0821a
Saahil Vashishta <97310378+saahilv@users.noreply.github.com>
2023-10-15 13:03:47 -0700

    Add react build files for deployment

---
+++ code/Frontend/react-frontend/build/precache-manifest.f018925f46e6b910754ea2af5c31c6fd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c002bded154c285a99735bb328325b93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6f0c448fc322186ebdb",
+    "url": "/static/css/2.c724e457.chunk.css"
+  },
+  {
+    "revision": "04102746ef5ae1e631de",
+    "url": "/static/css/main.70f65fee.chunk.css"
+  },
+  {
+    "revision": "b6f0c448fc322186ebdb",
+    "url": "/static/js/2.41b91d37.chunk.js"
+  },
+  {
+    "revision": "b87814b1015fe1cf39d19610c297ac05",
+    "url": "/static/js/2.41b91d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9db12dd22ad2fee972c",
+    "url": "/static/js/3.74082635.chunk.js"
+  },
+  {
+    "revision": "04102746ef5ae1e631de",
+    "url": "/static/js/main.72fede80.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.72fede80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7436230b27d53b0f1a49",
+    "url": "/static/js/runtime-main.2c4d2137.js"
+  },
+  {
+    "revision": "a49d1e1cf55880017274ab2fbe0bc05e",
+    "url": "/static/media/ANDLeader.a49d1e1c.jpg"
+  },
+  {
+    "revision": "e9095c75571bdb4895c4ac1752a29963",
+    "url": "/static/media/ConfigurationLeader.e9095c75.png"
+  },
+  {
+    "revision": "47b196213aeea686378bd1474862a7b4",
+    "url": "/static/media/GearOnTheGo.47b19621.png"
+  },
+  {
+    "revision": "5e2fdaea7cc37160b8e9df8f4f106980",
+    "url": "/static/media/QALeader.5e2fdaea.png"
+  },
+  {
+    "revision": "81b0d49ff134d844e93762c882baf17a",
+    "url": "/static/media/RequirementLeader.81b0d49f.jpeg"
+  },
+  {
+    "revision": "22f8125ad7803464e8c17957adcb1f20",
+    "url": "/static/media/SecurityLeader.22f8125a.jpg"
+  },
+  {
+    "revision": "8f244c0011ec4288d30f1960bf9a4214",
+    "url": "/static/media/TeamLeader.8f244c00.jpg"
+  }
+]);
